d04eafdc771b95c57cba98eba6ed2bbf0c3d0022
ramonzacarias <ramonsantana484@gmail.com>
2023-04-16 22:12:03 -0300

    Atualização estrutura de arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:minha_estante/screens/home.dart';
-import 'package:minha_estante/screens/login_screen.dart';
+
+
+import 'app.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha estante',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginScreen(),
-    );
-  }
-}
 
 
