23ab98c09f085ef3531b29ef4c65c3087a5c328b
ramonzacarias <ramonsantana484@gmail.com>
2023-04-18 22:22:36 -0300

    Criando a onboarding_page

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:minha_estante/features/splash/splash_page.dart';
+import 'package:minha_estante/features/onboarding/onboarding_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashPage()
+      home: OnboardingPage()
     );
   }
 }
--- lib/commom/constants/app_colors.dart
+++ lib/commom/constants/app_colors.dart
@@ -6,5 +6,17 @@ class AppColors {
   static const Color greenLightOne = Color(0xFF63B5AF);
   static const Color greenLightTwo = Color(0xFF438883);
 
+  static const List<Color> greenGradient = [
+    Color(0xFF63B5AF),
+    Color(0xFF438883),
+  ];
+  static const List<Color> greyGradient = [
+    Color(0xFFB5B5B5),
+    Color(0xFF7F7F7F),
+  ];
+
   static const Color white = Color(0xFFFFFFFF);
+  static const Color iceWhite = Color(0xFFEEF8F7);
+  static const Color grey = Color(0xFF444444);
+  
 }
