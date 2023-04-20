f7650e5b8d16af227702b003dbed191df227d52d
ramonzacarias <ramonsantana484@gmail.com>
2023-04-20 11:45:29 -0300

    Refatorando o código

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -14,60 +14,59 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.iceWhite,
-      body: Align(
-        child: Column(
-          children: [
-            const SizedBox(height: 48.0),
-            Expanded(
-              flex: 2,
-              child: Image.asset(
-                'assets/images/people.png',
-              ),
+      body: Column(
+        children: [
+          const SizedBox(height: 48.0),
+          Expanded(
+            child: Image.asset(
+              'assets/images/onboarding_image.png',
             ),
-            Text(
-              'Gerencie Sua Biblioteca',
-              style: AppTextStyles.bigText.copyWith(
-                color: AppColors.greenLightTwo,
-              ),
+          ),
+          Text(
+            'Gerencie Sua Biblioteca',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.bigText.copyWith(
+              color: AppColors.greenLightTwo,
             ),
-            Text(
-              'De Leitura',
-              style: AppTextStyles.bigText.copyWith(
-                color: AppColors.greenLightTwo,
-              ),
+          ),
+          Text(
+            'De Leitura',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.bigText.copyWith(
+              color: AppColors.greenLightTwo,
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 32.0,
-                right: 32.0,
-                top: 24.0,
-                bottom: 4.0,
-              ),
-              child: PrimaryButton(
-                text: 'Fazer Login',
-                onPressed: () {},
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 32.0,
+              right: 32.0,
+              top: 24.0,
+              bottom: 4.0,
             ),
-            MultiTextButton(
-              onPressed: () => log('tap') ,
-              children: [
-                Text(
-                  'Não tem uma conta? ',
-                  style: AppTextStyles.smallText.copyWith(
-                    color: AppColors.grey,
-                  ),
+            child: PrimaryButton(
+              text: 'Fazer Login',
+              onPressed: () {},
+            ),
+          ),
+          MultiTextButton(
+            onPressed: () => log('tap') ,
+            children: [
+              Text(
+                'Não tem uma conta? ',
+                style: AppTextStyles.smallText.copyWith(
+                  color: AppColors.grey,
                 ),
-                Text(
-                  'Criar Conta.',
-                  style: AppTextStyles.smallText.copyWith(
-                    color: AppColors.greenLightTwo,
-                  ),
+              ),
+              Text(
+                'Criar Conta.',
+                style: AppTextStyles.smallText.copyWith(
+                  color: AppColors.greenLightTwo,
                 ),
-              ],
-            ),
-            const SizedBox(height: 25.0)
-          ],
-        ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 25.0)
+        ],
       ),
     );
   }
