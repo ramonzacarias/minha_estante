c9ebddffd2ae7937dcef4eda59e9607f6f25815e
ramonzacarias <ramonsantana484@gmail.com>
2023-04-23 00:41:56 -0300

    Criando a conexão com o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:minha_estante/firebase_options.dart';
 import 'package:minha_estante/locator.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupDependencies(); // Fazendo a chamada para a criação das dependencias
   runApp(const App());
 }
