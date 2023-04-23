e431e79206b45a4b406c819720f9ddb40bfdf49d
ramonzacarias <ramonsantana484@gmail.com>
2023-04-23 01:34:24 -0300

    Implementação do Firebase Authentication

---
+++ lib/services/firebase_auth_service.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:minha_estante/commom/models/user_model.dart';
+import 'package:minha_estante/services/auth_service.dart';
+
+class FirebaseAuthService implements AuthService {
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Future<UserModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      // Verifica se o úsuario é null para realizar o seu registro
+      if (result.user != null) {
+        return UserModel(
+          name: result.user!.displayName,
+          email: result.user!.email,
+          id: result.user!.uid,
+        );
+      } else {
+        throw Exception();
+      }
+    } on FirebaseAuthException catch (e) {
+      throw e.message ?? "null";
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<UserModel> signUp({
+    String? name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      // Verifica se o úsuario é null para realizar o seu registro
+      if (result.user != null) {
+        await result.user!.updateDisplayName(name);
+        return UserModel(
+          name: result.user!.displayName,
+          email: result.user!.email,
+          id: result.user!.uid,
+        );
+      } else {
+        throw Exception();
+      }
+    } on FirebaseAuthException catch (e) {
+      throw e.message ?? "null";
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
