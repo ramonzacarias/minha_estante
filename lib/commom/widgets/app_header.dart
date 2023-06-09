b41248926a66bff31f8d373a5e73bda0f097aa3c
ramonzacarias <ramonsantana484@gmail.com>
2023-06-08 23:26:31 -0300

    Criação do widget AppHeader e ajustes na page perfil

---
+++ lib/commom/widgets/app_header.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:minha_estante/commom/constants/app_colors.dart';
+import 'package:minha_estante/commom/constants/app_text_styles.dart';
+import 'package:minha_estante/commom/constants/routes.dart';
+
+class AppHeader extends StatefulWidget {
+  final String? title;
+  const AppHeader({
+    super.key,
+    this.title,
+  });
+
+  @override
+  State<AppHeader> createState() => _AppHeaderState();
+}
+
+class _AppHeaderState extends State<AppHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Positioned(
+          left: 0,
+          right: 0,
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: AppColors.greenGradient,
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.elliptical(500, 30),
+                bottomRight: Radius.elliptical(500, 30),
+              ),
+            ),
+            height: 287,
+          ),
+        ),
+        Positioned(
+          left: 15,
+          right: 0,
+          top: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(
+                onTap: () =>
+                    Navigator.popAndPushNamed(context, NamedRoute.splash),
+                child: const Icon(
+                  Icons.arrow_back_ios,
+                  size: 25.0,
+                  color: AppColors.white,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  widget.title!,
+                  style: AppTextStyles.bigText.copyWith(color: AppColors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                width: 40.0,
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
