01b8de12c6437371607090774cd19f5fb652f81a
ramonzacarias <ramonsantana484@gmail.com>
2023-07-05 02:54:53 -0300

    Criando a classe de constantes para as coleções do firestore

---
+++ lib/commom/constants/firestore_colletions.dart
@@ -0,0 +1,8 @@
+// Constante com as coleções do firestore
+class FirestoreCollections {
+  FirestoreCollections._(); // Chamando as propriedades dentro do construtor FirestoreCollections()
+
+  static const String users = 'users';
+  static const String biblioteca = 'biblioteca';
+  static const String livros = 'livros';
+}
