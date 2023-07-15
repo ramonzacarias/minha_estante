fe74125f8c35cf495196aea33b454b1551f3233f
ramonzacarias <ramonsantana484@gmail.com>
2023-07-15 13:41:19 -0300

    Adicionando alguns comentarios na classe secure storege

--- lib/services/secure_storage.dart
+++ lib/services/secure_storage.dart
@@ -5,6 +5,7 @@ class SecureStorage {
 
   final _secureStorage = const FlutterSecureStorage();
 
+  // Escreve um valor no armazenamento seguro
   Future<void> write({required String key, String? value,}) async {
     await _secureStorage.write(
       key: key,
@@ -12,22 +13,27 @@ class SecureStorage {
     );
   }
 
+  // Lê um valor específico do armazenamento seguro
   Future<String?> readOne({required String key}) async {
     return await _secureStorage.read(key: key);
   }
 
+  // Lê todos os valores do armazenamento seguro
   Future<Map<String, String>> readAll() async {
     return await _secureStorage.readAll();
   }
 
+  // Exclui um valor específico do armazenamento seguro
   Future<void> deleteOne({required String key}) async {
     await _secureStorage.delete(key: key);
   }
 
+  // Exclui todos os valores do armazenamento seguro
   Future<void> deleteAll() async {
     await _secureStorage.deleteAll();
   }
 
+  // Atualiza um valor específico no armazenamento seguro
   Future<void> update({required String key, required String newValue}) async {
     await _secureStorage.write(
       key: key, 
